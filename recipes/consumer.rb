4781b48b743415dc66a705033df04c70df040f8b
Steffen Gebert <steffen.gebert@typo3.org>
2013-03-01 08:26:05 +0100

    Use pika 0.9.8

--- recipes/consumer.rb
+++ recipes/consumer.rb
@@ -5,6 +5,8 @@ include_recipe "runit"
 include_recipe "python"
 
 python_pip "pika" do
+  # 0.9.9 is buggy and consumes 100% cpu. 0.9.10 will include the fix, but is unreleased, yet
+  version "0.9.8"
   action :install
 end
 
