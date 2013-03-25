ef0e512f486ecabccd44b74a48f62ec751efdff8
Steffen Gebert <steffen.gebert@typo3.org>
2013-03-25 08:59:20 +0100

    Read credentials from data bags

--- recipes/consumer.rb
+++ recipes/consumer.rb
@@ -5,13 +5,34 @@ include_recipe "runit"
 include_recipe "python"
 
 python_pip "pika" do
-  # 0.9.9 is buggy and consumes 100% cpu. 0.9.10 will include the fix, but is unreleased, yet
-  version "0.9.8"
   action :install
 end
 
+# remove pre- from environment (if we're eg in pre-production, which almost equals production)
+environment = node.chef_environment.gsub(/pre-/, '')
+
+# get all passwords for this environment
+all_password_data = Chef::EncryptedDataBagItem.load("passwords", environment)
+
+# remove . from fqdn
+server_cleaned = node['site-svntypo3org']['amqp']['server'].gsub(/\./, "")
+user = node['site-svntypo3org']['amqp']['user']
+
+Chef::Log.info "Looking for password rabbitmq.#{server_cleaned}.#{}"
+if all_password_data["rabbitmq"][server_cleaned][user].nil?
+  raise "Cannot find password for rabbitmq user '#{user}' in data bag 'passwords/#{node["chef_environment"]}'."
+end
+
+amqp_pass = all_password_data["rabbitmq"][server_cleaned][user]
+
 runit_service "mq-consumer-svn-create" do
   default_logger true
+  options({
+    :amqp_server   => node['site-svntypo3org']['amqp']['server'],
+    :amqp_user     => node['site-svntypo3org']['amqp']['user'],
+    :amqp_vhost    => node['site-svntypo3org']['amqp']['vhost'],
+    :amqp_password => amqp_pass
+  })
 end
 
 directory "/var/log/mq-consumer-svn-create"
