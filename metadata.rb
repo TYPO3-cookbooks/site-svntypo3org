ec691bb05c4b0371bae380606ce0544882529122
Steffen Gebert <steffen.gebert@typo3.org>
2013-11-27 22:36:09 +0100

    FC011: Missing README in markdown format

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer       "TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures svn.typo3.org"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
 depends "ssl_certificates"
