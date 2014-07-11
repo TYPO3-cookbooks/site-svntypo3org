9221dab206e8c1708e798356d23b55989928094c
Michael Stucki <michael.stucki@typo3.org>
2014-07-11 18:55:06 +0200

    Add dependency on t3-chef-vault

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,12 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures svn.typo3.org"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
 depends "ssl_certificates"
 depends "apache2"
 depends "chef-vault"
+depends "t3-chef-vault"
 
 # for rabbitmq consumer
 depends "runit"
